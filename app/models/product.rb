f7a9ac1c605dd6d3073619598761c3f490f04a48
bubsworth <bubbard96@hotmail.com>
2025-03-06 16:59:30 +0000

    Iteration D2: Connecting Products to Carts

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   has_one_attached :image
   after_commit -> { broadcast_refresh_later_to "products" }
 
@@ -14,5 +16,15 @@ class Product < ApplicationRecord
       errors.add(:image, "must be a GIF, JPG or PNG image")
     end
   end
+
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  private
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, "Line Items present")
+        throw :abort
+      end
+    end
 end
