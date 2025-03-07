cbb491c1a15e814b7f3a4f5569f9f8e33df7b6dd
bubsworth <bubbard96@hotmail.com>
2025-03-07 10:07:10 +0000

    Iteration F3: Highlighting Changes

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -21,14 +21,14 @@ class LineItemsController < ApplicationController
   def edit
   end
 
-  # POST /line_items or /line_items.json
+  # POST /line_items or /line_items.jsonp
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        format.turbo_stream
+        format.turbo_stream { @current_item = @line_item }
         format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
